4832f7ff12dd74110d5f2f5760df33571642c736
madalilng <iam.renalcantara@yahoo.com.ph>
2014-12-31 03:06:43 -0800

    spell factory level 5

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	$towerName = array("canonT", "archerT", "mortarT","airT","wizardT","teslaT","xbowT","infernoT","campT","elixpumpT","elixstorT","goldpumpT","goldstorT","barrackT","laboratoryT","castT","spellT","darkpumpT","darkstorT","darkbarrackT");
 	$towerID = array("1000008", "1000009", "1000013","1000012","1000011","1000019","1000021","1000027","1000000","1000002","1000003","1000004","1000005","1000006","1000007","1000014","1000020","1000023","1000024","1000026");
-	$towerMaxLVL = array(12,13,8,8,8,8,4,3,8,11,11,12,11,10,8,6,4,6,6,6);
+	$towerMaxLVL = array(12,13,8,8,8,8,4,3,8,11,11,12,11,10,8,6,5,6,6,6);
 	$towerIMG = array(158,159,160,161,162,163,164,165,180,176,175,173,174,181,183,188,184,177,178,182);
 	$towerSize = array(3,3,3,3,3,2,3,2,5,3,3,3,3,3,4,3,3,3,3,3,3);
 ?>
